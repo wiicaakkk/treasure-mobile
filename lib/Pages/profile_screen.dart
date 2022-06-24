05180577191d3dadef597ce3ed580eeb1653ecfd
KhairatHayati <103499296+KhairatHayati@users.noreply.github.com>
2022-06-25 06:49:34 +0700

    Add files via upload

---
+++ lib/Pages/profile_screen.dart
@@ -0,0 +1,165 @@
+import 'package:animated_theme_switcher/animated_theme_switcher.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
+import 'package:treasure/theme.dart';
+import 'package:treasure/widgets/profile_list_item.dart';
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    ScreenUtil.init(context);
+    var profileInfo = Expanded(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: kSpacingUnit.w * 10,
+            width: kSpacingUnit.w * 10,
+            margin: EdgeInsets.only(top: kSpacingUnit.w * 3),
+            child: Stack(
+              children: <Widget>[
+                CircleAvatar(
+                  radius: kSpacingUnit.w * 5,
+                  backgroundImage: const AssetImage('assets/images/user1.png'),
+                ),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: Container(
+                    height: kSpacingUnit.w * 2.5,
+                    width: kSpacingUnit.w * 2.5,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).accentColor,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Center(
+                      heightFactor: kSpacingUnit.w * 1.5,
+                      widthFactor: kSpacingUnit.w * 1.5,
+                      child: Icon(
+                        LineAwesomeIcons.pen,
+                        color: kPrimaryColor,
+                        size: ScreenUtil().setSp(kSpacingUnit.w * 1.5),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: kSpacingUnit.w * 2),
+          Text(
+            'Nicolas Adams',
+            style: titleText,
+          ),
+          SizedBox(height: kSpacingUnit.w * 0.5),
+          Text(
+            'nicolasadams@gmail.com',
+            style: titleText,
+          ),
+          SizedBox(height: kSpacingUnit.w * 2),
+          Container(
+            height: kSpacingUnit.w * 4,
+            width: kSpacingUnit.w * 20,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(kSpacingUnit.w * 3),
+              color: Theme.of(context).accentColor,
+            ),
+            child: Center(
+              child: Text(
+                'Upgrade to PRO',
+                style: textButton,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+
+    var themeSwitcher = ThemeSwitcher(
+      builder: (context) {
+        return AnimatedCrossFade(
+          duration: const Duration(milliseconds: 200),
+          crossFadeState: Theme.of(context).brightness == Brightness.dark
+              ? CrossFadeState.showFirst
+              : CrossFadeState.showSecond,
+          firstChild: GestureDetector(
+            onTap: () =>
+                ThemeSwitcher.of(context).changeTheme(theme: darkTheme),
+            child: Icon(
+              LineAwesomeIcons.sun,
+              size: ScreenUtil().setSp(kSpacingUnit.w * 3),
+            ),
+          ),
+          secondChild: GestureDetector(
+            onTap: () =>
+                ThemeSwitcher.of(context).changeTheme(theme: darkTheme),
+            child: Icon(
+              LineAwesomeIcons.moon,
+              size: ScreenUtil().setSp(kSpacingUnit.w * 3),
+            ),
+          ),
+        );
+      },
+    );
+
+    var header = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(width: kSpacingUnit.w * 3),
+        Icon(
+          LineAwesomeIcons.arrow_left,
+          size: ScreenUtil().setSp(kSpacingUnit.w * 3),
+        ),
+        profileInfo,
+        themeSwitcher,
+        SizedBox(width: kSpacingUnit.w * 3),
+      ],
+    );
+
+    return ThemeSwitchingArea(
+      child: Builder(
+        builder: (context) {
+          return Scaffold(
+            body: Column(
+              children: <Widget>[
+                SizedBox(height: kSpacingUnit.w * 5),
+                header,
+                Expanded(
+                  child: ListView(
+                    children: <Widget>[
+                      ProfileListItem(
+                          icon: LineAwesomeIcons.user_shield,
+                          text: 'Privacy',
+                          hasNavigation: false),
+                      ProfileListItem(
+                          icon: LineAwesomeIcons.history,
+                          text: 'Purchase History',
+                          hasNavigation: false),
+                      ProfileListItem(
+                          icon: LineAwesomeIcons.question_circle,
+                          text: 'Help & Support',
+                          hasNavigation: false),
+                      ProfileListItem(
+                          icon: LineAwesomeIcons.cog,
+                          text: 'Settings',
+                          hasNavigation: false),
+                      ProfileListItem(
+                          icon: LineAwesomeIcons.user_plus,
+                          text: 'Invite a Friend',
+                          hasNavigation: false),
+                      ProfileListItem(
+                        icon: LineAwesomeIcons.alternate_sign_out,
+                        text: 'Logout',
+                        hasNavigation: false,
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
